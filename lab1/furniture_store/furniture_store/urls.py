e579a8ead18219d49e16d56d2ab6ebf43f2cb826
sibastr <49680365+sibastr@users.noreply.github.com>
2021-02-05 21:22:58 +0300

    Swagger DONE

--- lab1/furniture_store/furniture_store/urls.py
+++ lab1/furniture_store/furniture_store/urls.py
@@ -3,6 +3,11 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 
 urlpatterns = [
@@ -13,8 +18,22 @@ urlpatterns = [
     path('', include('reviews.urls'))
 ]
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Online-shop API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
